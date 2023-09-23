74153dc25e28c0591f55c7e34d227a346163cb89
ayushkarwal <ayushkarwal@gmail.com>
2023-09-23 17:01:01 +0530

    Changes related to VUE.js for todo appn.

---
+++ Django_with_vue/TODO_in_VUE/todo/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <H3>MY TODO APP</H3>
+  <TaskNew v-for="task in tasks" :key="task.id" :task="task"></TaskNew>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TaskNew from './components/Task.vue'
+export default {
+  name: 'App',
+  components: {
+    TaskNew
+  },
+  data: () => {
+    return{
+      msg: "Hello Kumkum!", 
+      tasks: [
+      {
+        id:1,
+        title: "Task 1",
+        priority: true,
+        completed: false,
+      },
+      {
+        id:2,
+        title: "Task 2",
+        priority: true,
+        completed: false,
+      },
+      { id:3,
+        title: "Task 3",
+        priority: false,
+        completed: false,
+      }
+    ]
+    }
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Django_with_vue/TODO_in_VUE/todo/src/components/Task.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="box" :class="task.priority?'priority_task':'normal_task'">
+    <p>{{ task.title }}</p>
+    <img v-show="task.completed" src="../assets/icon_tick.png" style="width: 20px; height: 20px;" alt="">
+    <button v-on:click="set_as_completed(task)">Completed</button>
+    <button>Deleted</button>
+    <button @click="set_as_priority(task)">Set as priority</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+    name: 'TaskNew',
+    props: ["task"],
+    methods:{
+        set_as_priority(task) {
+            task.priority = !task.priority
+            console.log("task set as priority")
+        },
+        set_as_completed(task){
+            task.completed = !task.completed
+        }
+    }
+}
+</script>
+
+<style>
+    .box{
+        width: 300px;
+        border:2px;
+        background-color: aqua;
+    }
+    .box.priority_task {
+        background-color: yellow;
+    }
+</style>
